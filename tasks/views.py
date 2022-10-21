f4edeb3efa9e0cba582fd23c0c533e16dc5b43a2
Renan Z <zumpchiat@gmail.com>
2022-10-20 23:15:15 -0300

    initial commit

---
+++ tasks/views.py
@@ -0,0 +1,8 @@
+import http
+from http.client import HTTPResponse
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def hello(request):
+    return HttpResponse('oioioio')
