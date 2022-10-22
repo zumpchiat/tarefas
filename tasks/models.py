5be584f3170b2b4c243b9dd68d7447f91256010b
Renan Z <zumpchiat@gmail.com>
2022-10-22 16:46:07 -0300

    layout and model task

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth import admin
 from django.db import models
 
-# Create your models here.
+
+STATUS = (
+    ('doing', 'Doing'),
+    ('dona', 'Done')
+)
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    done = models.CharField(max_length=5, choices=STATUS)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+
+
